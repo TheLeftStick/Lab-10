8e783a9963264d79b4055695a51d6423f8e04abe
Cristian Balan <balanc@umich.edu>
2021-12-09 15:22:10 -0500

    merging methods

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -62,12 +62,46 @@ public class testSelectionSort {
     }
 
     public void testMixed(){
+			int[] arr = new int[5];
+			arr[0] = 2;
+			arr[1] = 0;
+			arr[2] = 3;
+			arr[3] = -1;
+			arr[4] = 1;
 
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -1;
+			Sortedarr[1] = 0;
+			Sortedarr[2] = 1;
+			Sortedarr[3] = 2;
+			Sortedarr[4] = 3;
+
+			SelectionSort temp1 = new SelectionSort();
+			arr = temp1.basicSelectionSort(arr);
+
+assertArrayEquals(arr, Sortedarr);
         /** Test data contains with both positive, negative and zeros **/
     }
 
     public void testDuplicates(){
+			int[] arr = new int[5];
+			arr[0] = 2;
+			arr[1] = -1;
+			arr[2] = 2;
+			arr[3] = 10;
+			arr[4] = -1;
 
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -1;
+			Sortedarr[1] = -1;
+			Sortedarr[2] = 2;
+			Sortedarr[3] = 2;
+			Sortedarr[4] = 10;
+
+			SelectionSort temp1 = new SelectionSort();
+			arr = temp1.basicSelectionSort(arr);
+
+			assertArrayEquals(arr, Sortedarr);
 
         /** Test data contains duplicates **/
     }
